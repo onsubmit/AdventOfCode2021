662cb9e478680f86292c5ea9d888dafe200d5f1c
Andy <onsubmit@outlook.com>
2021-12-02 18:32:02 -0800

    Add day 1

---
+++ Days/Day01.cs
@@ -0,0 +1,40 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Day01.cs" company="Andy Young">
+//     Copyright (c) Andy Young. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace AdventOfCode2021.Days
+{
+    /// <summary>
+    /// Calculates the solution for the particular day.
+    /// </summary>
+    internal class Day01 : IDay
+    {
+        /// <summary>
+        /// Gets the number of depth increases.
+        /// </summary>
+        /// <returns>The number of depth increases.</returns>
+        /// <exception cref="InvalidOperationException">Thrown when the depth readings are not found.</exception>
+        public string GetSolution()
+        {
+            int increases = 0;
+
+            using StreamReader sr = new StreamReader("input\\Day01.txt");
+            string? previousLine = sr.ReadLine();
+            string? currentLine = null;
+            while ((currentLine = sr.ReadLine()) != null)
+            {
+                if (int.TryParse(previousLine, out int previousDepth)
+                    && int.TryParse(currentLine, out int currentDepth)
+                    && currentDepth > previousDepth)
+                {
+                    increases++;
+                }
+
+                previousLine = currentLine;
+            }
+
+            return increases.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="Program.cs" company="Andy Young">
+// Copyright (c) Andy Young. All rights reserved.
+// </copyright>
+
+namespace AdventOfCode2021 // Note: actual namespace depends on the project name.
+{
+    using System;
+    using AdventOfCode2021.Days;
+
+    /// <summary>
+    /// Program class.
+    /// </summary>
+    public class Program
+    {
+        private static readonly IDay[] Days = GetDays();
+
+        /// <summary>
+        /// Main entry point.
+        /// </summary>
+        public static void Main()
+        {
+            int totalLength = Days.Length.ToString().Length;
+            for (int i = 0; i < Days.Length; i++)
+            {
+                string dayNumber = (i + 1).ToString().PadLeft(totalLength, '0');
+                Console.WriteLine($"Day {dayNumber}: {Days[i].GetSolution()}");
+            }
+        }
+
+        /// <summary>
+        /// Gets an array containing an instance of each day.
+        /// </summary>
+        /// <returns>An array containing an instance of each day.</returns>
+        /// <exception cref="InvalidCastException">Thrown if instance creation of any day fails.</exception>
+        private static IDay[] GetDays()
+        {
+            Type dayInterfaceType = typeof(IDay);
+            return AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(assembly => assembly.GetTypes())
+                .Where(type => type != dayInterfaceType && dayInterfaceType.IsAssignableFrom(type))
+                .Select(type => Activator.CreateInstance(type) as IDay ?? throw new InvalidCastException()).ToArray();
+        }
+    }
+}
