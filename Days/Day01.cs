7a5f44af4ade6676e912a6cdb7d1721aed727c1c
Andy <onsubmit@outlook.com>
2021-12-02 22:17:49 -0800

    Solve day 1 part 2

--- Days/Day01.cs
+++ Days/Day01.cs
@@ -17,23 +17,90 @@ namespace AdventOfCode2021.Days
         public string GetSolution()
         {
             int increases = 0;
+            int windowSize = 3;
 
-            using StreamReader sr = new StreamReader("input\\Day01.txt");
-            string? previousLine = sr.ReadLine();
-            string? currentLine = null;
-            while ((currentLine = sr.ReadLine()) != null)
+            using StreamReader sr = new("input\\Day01.txt");
+
+            // TODO: Write a general solution that works for any window size.
+            if (windowSize == 1)
             {
-                if (int.TryParse(previousLine, out int previousDepth)
-                    && int.TryParse(currentLine, out int currentDepth)
-                    && currentDepth > previousDepth)
+                string? previousLine = sr.ReadLine();
+                string? currentLine = null;
+                while ((currentLine = sr.ReadLine()) != null)
+                {
+                    if (int.TryParse(previousLine, out int previousDepth)
+                        && int.TryParse(currentLine, out int currentDepth)
+                        && currentDepth > previousDepth)
+                    {
+                        increases++;
+                    }
+
+                    previousLine = currentLine;
+                }
+
+                return increases.ToString();
+            }
+
+            List<int> window = GetWindow(sr, (2 * windowSize) - 1);
+
+            int index = 0;
+            int previousSum = 0;
+            bool previousSumKnown = false;
+            while (window.Count == (2 * windowSize) - 1)
+            {
+                int currentSum = 0;
+                for (int i = index; i < index + windowSize; i++)
+                {
+                    if (!previousSumKnown)
+                    {
+                        previousSum += window[i];
+                    }
+
+                    currentSum += window[i + 1];
+                }
+
+                if (currentSum > previousSum)
                 {
                     increases++;
                 }
 
-                previousLine = currentLine;
+                index++;
+                previousSum = currentSum;
+                previousSumKnown = true;
+
+                if (index + windowSize >= window.Count)
+                {
+                    window = window.GetRange(windowSize, window.Count - windowSize);
+                    window.AddRange(GetWindow(sr, windowSize));
+                    index = -1;
+                }
             }
 
             return increases.ToString();
+        }
+
+        /// <summary>
+        /// Gets the measurement window of size <paramref name="windowSize"/>.
+        /// </summary>
+        /// <param name="sr">The <see cref="StreamReader"/> for the depth input file.</param>
+        /// <param name="windowSize">The size of the window.</param>
+        /// <returns>A list of depth of readings. Can return incomplete windows.</returns>
+        private static List<int> GetWindow(StreamReader sr, int windowSize)
+        {
+            List<int> window = new();
+            for (int i = 0; i < windowSize; i++)
+            {
+                string? line = sr.ReadLine();
+
+                if (line == null || !int.TryParse(line, out int depth))
+                {
+                    return window;
+                }
+
+                window.Add(depth);
+            }
+
+            return window;
         }
     }
 }
