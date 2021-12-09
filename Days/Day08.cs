a61b48306f6b1b510a2867c54c1445523ff6e640
Andy <onsubmit@outlook.com>
2021-12-08 19:24:57 -0800

    Solve day 8 part 1

---
+++ Days/Day08.cs
@@ -0,0 +1,53 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Day08.cs" company="Andy Young">
+//     Copyright (c) Andy Young. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace AdventOfCode2021.Days
+{
+    using AdventOfCode2021.Models;
+
+    /// <summary>
+    /// Calculates the solution for the particular day.
+    /// </summary>
+    internal class Day08 : IDay
+    {
+        /// <summary>
+        /// Calculates the solution for the particular day.
+        /// </summary>
+        /// <returns>The solution.</returns>
+        public string GetSolution()
+        {
+            int[] numSegments = new int[10] { 6, 2, 5, 5, 4, 5, 6, 3, 7, 6 };
+            int[] uniqueSegmentAmounts = new int[4] { 2, 3, 4, 7 };
+
+            using StreamReader sr = new("input\\Day08.txt");
+            string? line = null;
+
+            int numUniqueNumbers = 0;
+            while ((line = sr.ReadLine()) != null)
+            {
+                SevenSegmentDisplayEntry entry = GetSevenSegmentDisplayEntry(line);
+                numUniqueNumbers += entry.OutputValue.Count(v => uniqueSegmentAmounts.Contains(v.Length));
+            }
+
+            return numUniqueNumbers.ToString();
+        }
+
+        private static SevenSegmentDisplayEntry GetSevenSegmentDisplayEntry(string line)
+        {
+            string[] split = line.Split('|');
+            if (split.Length != 2)
+            {
+                throw new InvalidOperationException("Bad input");
+            }
+
+            return new()
+            {
+                SignalPatterns = new(split[0].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries)),
+                OutputValue = new(split[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries)),
+            };
+        }
+    }
+}
---
+++ Models/SevenSegmentDisplayEntry.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SevenSegmentDisplayEntry.cs" company="Andy Young">
+//     Copyright (c) Andy Young. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace AdventOfCode2021.Models
+{
+    /// <summary>
+    /// Represents a 7-segment display entry.
+    /// </summary>
+    internal record struct SevenSegmentDisplayEntry
+    {
+        /// <summary>
+        /// Gets or sets the signal patterns.
+        /// </summary>
+        public List<string> SignalPatterns { get; set; }
+
+        /// <summary>
+        /// Gets or sets the output value.
+        /// </summary>
+        public List<string> OutputValue { get; set; }
+
+        /// <summary>
+        /// Generates a string-representation of a <see cref="SevenSegmentDisplayEntry"/>.
+        /// </summary>
+        /// <returns>A string-representation of a <see cref="SevenSegmentDisplayEntry"/>.</returns>
+        public override string ToString()
+        {
+            return $"{string.Join(" ", this.SignalPatterns)} | {string.Join(" ", this.OutputValue)}";
+        }
+    }
+}
